5417c2eaabe260a0d113891dece1d26c63a21446
Rafal Janicki <rafal@kult.io>
2025-05-06 13:30:28 +0200

    feat: Add GoatCounter diagnostic logging in App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, useLocation } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import NotFound from "@/pages/not-found";
 import HomePage from "@/pages/HomePage";
@@ -7,6 +7,7 @@ import ForOwnersPage from "@/pages/ForOwnersPage";
 import BlogListPage from "@/pages/BlogListPage";
 import BlogPostPage from "@/pages/BlogPostPage";
 import { HelmetProvider } from 'react-helmet-async';
+import { useEffect } from 'react';
 
 function Router() {
   return (
@@ -22,6 +23,18 @@ function Router() {
 }
 
 function App() {
+  const [location] = useLocation();
+
+  useEffect(() => {
+    console.log('App useEffect: location changed to', location);
+    if (window.goatcounter) {
+      console.log('GoatCounter script loaded, attempting to count page view');
+      window.goatcounter.count();
+    } else {
+      console.log('GoatCounter script not loaded (window.goatcounter is undefined)');
+    }
+  }, [location]); // Re-run effect when location changes
+
   return (
     <HelmetProvider>
       <div className="min-h-screen flex flex-col">
